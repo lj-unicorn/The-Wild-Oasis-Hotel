ea3d7e13b014b0b7b9f56b3fe915af72f05cd47a
Lalit Chaudhary <230908665+lj-unicorn@users.noreply.github.com>
2025-09-06 00:33:58 +0545

    Enhance header with user avatar and account menu
    - Added UserAvatar component to display logged-in user's profile picture
    - Introduced HeaderMenu with account navigation and logout option
    - Updated Header layout with flexbox for proper alignment

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,17 +1,24 @@
 import Logout from "@/features/authentication/Logout.jsx";
 import React from "react";
 import styled from "styled-components";
+import HeaderMenu from "./HeaderMenu.jsx";
+import UserAvatar from "@/features/authentication/UserAvatar.jsx";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
